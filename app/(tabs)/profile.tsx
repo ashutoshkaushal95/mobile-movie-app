777b4b018b3c35319385fbfea47670342bbe4062
ashutoshkaushal95 <ashutoshkaushal2@gmail.com>
2025-03-29 12:31:54 +0530

    intital code ready with navbar

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const profile = () => {
   return (
-    <View>
+    <View className="flex-1 justify-center items-center">
       <Text>profile</Text>
     </View>
   );
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 export default function RootLayout() {
   return (
     <Stack>
+      {/* screen specific styling */}
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
     </Stack>
